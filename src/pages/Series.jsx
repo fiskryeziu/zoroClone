26a90f7fb3658a133929ebba7e82ba3f778474ff
fiskryeziu <fiskryeziu@gmail.com>
2022-12-10 18:29:24 +0100

    tvseries page style

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import Card from '../components/Card/Card'
+import Footer from '../components/Footer/Footer'
+import GenreCard from '../components/GenreCard/GenreCard'
+import { M } from '../components/MainContainer/maincontainer.style'
+import MostViewedCard from '../components/MostViewedCard/MostViewedCard'
+import NavBar from '../components/NavBar/NavBar'
+import ShareButton from '../components/ShareButton'
 
 const Series = () => {
-  return <div>Series</div>
+  return (
+    <>
+      <NavBar />
+      <ShareButton borderRadius={false} />
+      <M.MainWrapper>
+        <M.Main>
+          <M.Heading>TV Series Anime</M.Heading>
+          <M.MovieList>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(
+              (item, idx) => (
+                <Card key={idx} />
+              )
+            )}
+          </M.MovieList>
+        </M.Main>
+        <M.Aside>
+          <M.Heading>Most Viewed</M.Heading>
+          <MostViewedCard />
+          <M.Heading>Genres</M.Heading>
+          <GenreCard />
+        </M.Aside>
+      </M.MainWrapper>
+      <Footer />
+    </>
+  )
 }
 
 export default Series
