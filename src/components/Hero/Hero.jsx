764a82cf70c558c543fcf5e59e5d24db9c528338
Manjot Singh <sin13014.sheridancollege.ca>
2023-07-17 11:47:38 -0400

    Hero component has description and has style updates resembling new Aniwatch.to

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,89 +1,90 @@
-import React from 'react'
+import React from "react";
 import {
   FaCalendar,
   FaChevronLeft,
   FaChevronRight,
   FaClock,
   FaPlayCircle,
-} from 'react-icons/fa'
+} from "react-icons/fa";
 import SwiperCore, {
   Navigation,
   Pagination,
   Scrollbar,
   A11y,
   Autoplay,
-} from 'swiper'
-import 'swiper/swiper-bundle.css'
-import { H } from './hero.style'
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay])
-import useTrendingAnime from '../../hooks/useTrendingAnime'
-import { format } from 'date-fns'
+} from "swiper";
+import "swiper/swiper-bundle.css";
+import { H } from "./hero.style";
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
+import useTrendingAnime from "../../hooks/useTrendingAnime";
+import { format } from "date-fns";
 
 const Hero = () => {
-  const { data, isFetched } = useTrendingAnime()
+  const { data, isFetched } = useTrendingAnime();
 
   return (
-    <H.Swiper
-      slidesPerView={1}
-      pagination={{
-        clickable: true,
-      }}
-      direction="horizontal"
-      loop={true}
-      autoplay={{ delay: 3000 }}
-      modules={[Pagination]}
-      className="swiper"
-      navigation={{
-        nextEl: '.btn-next',
-        prevEl: '.btn-prev',
-      }}
-    >
-      {isFetched &&
-        data.data.map((item, idx) => (
-          <H.Slides key={idx}>
-            <H.ImgContainer>
-              <H.Img src={item.attributes.coverImage.large} />
-            </H.ImgContainer>
-            <H.Content>
-              <H.Rank>
-                <p>#{idx + 1} Spotlight</p>
-              </H.Rank>
-              <H.Title>{item.attributes.canonicalTitle}</H.Title>
-              <H.Icons>
-                <H.Icon>
-                  <FaPlayCircle size={12} />
-                  {item.attributes.showType}
-                </H.Icon>
-                <H.Icon>
-                  <FaClock size={12} /> {item.attributes.episodeLength}m
-                </H.Icon>
-                <H.Icon>
-                  <FaCalendar size={12} />
-                  {format(new Date(item.attributes.startDate), 'MMM d, y')}
-                </H.Icon>
-                <H.IconSpan>HD</H.IconSpan>
-              </H.Icons>
-              <H.Description>{item.description}</H.Description>
-              <H.WatchBtn>
-                <H.WatchLink to="/watch">
-                  <FaPlayCircle />
-                  Watch Now
-                </H.WatchLink>
-                <H.DetailLink to="/detail">
-                  Detail <FaChevronRight size={12} />
-                </H.DetailLink>
-              </H.WatchBtn>
-            </H.Content>
-          </H.Slides>
-        ))}
-      <div className="btn-prev">
-        <FaChevronLeft />
-      </div>
-      <div className="btn-next">
-        <FaChevronRight />
-      </div>
-    </H.Swiper>
-  )
-}
+      <H.Swiper
+          slidesPerView={1}
+          pagination={{
+            clickable: true,
+          }}
+          direction="horizontal"
+          loop={true}
+          autoplay={{ delay: 3000 }}
+          modules={[Pagination]}
+          className="swiper"
+          navigation={{
+            nextEl: ".btn-next",
+            prevEl: ".btn-prev",
+          }}
+      >
+        {isFetched &&
+            data.data.map((item, idx) => (
+                <H.Slides key={idx}>
+                  <H.ImgContainer>
+                    <H.Img src={item.attributes.coverImage.large} />
+                  </H.ImgContainer>
+                  <H.Content>
+                    <H.Rank>
+                      <p>#{idx + 1} Spotlight</p>
+                    </H.Rank>
 
-export default Hero
+                    <H.Title>{item.attributes.canonicalTitle}</H.Title>
+                    <H.Icons>
+                      <H.Icon>
+                        <FaPlayCircle size={12} />
+                        {item.attributes.showType}
+                      </H.Icon>
+                      <H.Icon>
+                        <FaClock size={12} /> {item.attributes.episodeLength}m
+                      </H.Icon>
+                      <H.Icon>
+                        <FaCalendar size={12} />
+                        {format(new Date(item.attributes.startDate), "MMM d, y")}
+                      </H.Icon>
+                      <H.IconSpan>HD</H.IconSpan>
+                    </H.Icons>
+                    <H.Description>{item.attributes.synopsis}</H.Description>
+                    <H.WatchBtn>
+                      <H.WatchLink to="/watch">
+                        <FaPlayCircle />
+                        Watch Now
+                      </H.WatchLink>
+                      <H.DetailLink to="/detail">
+                        Detail <FaChevronRight size={12} />
+                      </H.DetailLink>
+                    </H.WatchBtn>
+                  </H.Content>
+                </H.Slides>
+            ))}
+        <div className="btn-prev">
+          <FaChevronLeft />
+        </div>
+        <div className="btn-next">
+          <FaChevronRight />
+        </div>
+      </H.Swiper>
+  );
+};
+
+export default Hero;
--- src/components/Hero/hero.style.js
+++ src/components/Hero/hero.style.js
@@ -1,14 +1,13 @@
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import { Swiper, SwiperSlide } from 'swiper/react'
-
-export const H = {}
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+export const H = {};
 
 H.Swiper = styled(Swiper)`
   position: relative;
   display: flex;
   width: 100%;
-  height: 80vh;
+  height: 60vh;
   overflow: hidden;
   z-index: 1;
 
@@ -21,13 +20,13 @@ H.Swiper = styled(Swiper)`
   @media screen and (max-width: 479px) {
     height: 50vh;
   }
-`
+`;
 
-H.Slides = styled(SwiperSlide)``
+H.Slides = styled(SwiperSlide)``;
 
 H.ImgContainer = styled.div`
   &::before {
-    content: '';
+    content: "";
     z-index: 1;
     position: absolute;
     top: 0;
@@ -36,33 +35,33 @@ H.ImgContainer = styled.div`
     bottom: 0;
     background: #202125;
     background: -moz-linear-gradient(
-      90deg,
-      #202125 0,
-      rgba(32, 33, 37, 0.6) 40%,
-      rgba(32, 33, 37, 0) 60%,
-      rgba(32, 33, 37, 0) 80%,
-      #202125 100%
+            90deg,
+            #202125 0,
+            rgba(32, 33, 37, 0.6) 40%,
+            rgba(32, 33, 37, 0) 60%,
+            rgba(32, 33, 37, 0) 80%,
+            #202125 100%
     );
     background: -webkit-linear-gradient(
-      90deg,
-      #202125 0,
-      rgba(32, 33, 37, 0.6) 40%,
-      rgba(32, 33, 37, 0) 60%,
-      rgba(32, 33, 37, 0) 80%,
-      #202125 100%
+            90deg,
+            #202125 0,
+            rgba(32, 33, 37, 0.6) 40%,
+            rgba(32, 33, 37, 0) 60%,
+            rgba(32, 33, 37, 0) 80%,
+            #202125 100%
     );
     background: linear-gradient(
-      90deg,
-      #202125 0,
-      rgba(32, 33, 37, 0.6) 40%,
-      rgba(32, 33, 37, 0) 60%,
-      rgba(32, 33, 37, 0) 80%,
-      #202125 100%
+            90deg,
+            #202125 0,
+            rgba(32, 33, 37, 0.6) 40%,
+            rgba(32, 33, 37, 0) 60%,
+            rgba(32, 33, 37, 0) 80%,
+            #202125 100%
     );
   }
 
   &::after {
-    content: '';
+    content: "";
     z-index: 1;
     position: absolute;
     top: 0;
@@ -71,58 +70,61 @@ H.ImgContainer = styled.div`
     bottom: -1px;
     background: #202125;
     background: -moz-linear-gradient(
-      0deg,
-      #202125 0,
-      rgba(32, 33, 37, 0) 50%,
-      #202125 100%
+            0deg,
+            #202125 0,
+            rgba(32, 33, 37, 0) 50%,
+            #202125 100%
     );
     background: -webkit-linear-gradient(
-      0deg,
-      #202125 0,
-      rgba(32, 33, 37, 0) 50%,
-      #202125 100%
+            0deg,
+            #202125 0,
+            rgba(32, 33, 37, 0) 50%,
+            #202125 100%
     );
     background: linear-gradient(
-      0deg,
-      #202125 0,
-      rgba(32, 33, 37, 0) 50%,
-      #202125 100%
+            0deg,
+            #202125 0,
+            rgba(32, 33, 37, 0) 50%,
+            #202125 100%
     );
   }
   @media screen and (max-width: 759px) {
     &::after {
       background: #202125;
       background: -moz-linear-gradient(
-        0deg,
-        #202125 0,
-        rgba(32, 33, 37, 0) 82%
+              0deg,
+              #202125 0,
+              rgba(32, 33, 37, 0) 82%
       );
       background: -webkit-linear-gradient(
-        0deg,
-        #202125 0,
-        rgba(32, 33, 37, 0) 82%
+              0deg,
+              #202125 0,
+              rgba(32, 33, 37, 0) 82%
       );
       background: linear-gradient(0deg, #202125 0, rgba(32, 33, 37, 0) 82%);
     }
     &::before {
-      content: '';
+      content: "";
       opacity: 0;
     }
   }
-`
+`;
 
 H.Img = styled.img`
-  width: 100%;
+  width: 70%;
   height: 100%;
   position: absolute;
+  left: 30%;
   object-fit: cover;
   @media screen and (max-width: 759px) {
-    opacity: 0.6;
+    opacity: 0.4;
+    width: 100%;
+    left: 0;
   }
-`
+`;
 H.Content = styled.div`
   z-index: 1;
-  width: 600px;
+  width: 30%;
   height: 100%;
   position: absolute;
   display: flex;
@@ -132,8 +134,9 @@ H.Content = styled.div`
   left: 2em;
   bottom: 2em;
   padding-bottom: 3em;
-
   bottom: 0 !important;
+  background: rgba(0, 0, 0, 0.9);
+  box-shadow: 30px 0 100px 100px rgba(0, 0, 0, 1);
 
   @media screen and (max-width: 1200px) {
     width: 50%;
@@ -141,23 +144,29 @@ H.Content = styled.div`
   @media screen and (max-width: 759px) {
     width: 100%;
     padding-right: 100px;
+    box-shadow: none;
+    background: rgb(0, 0, 0, 0);
   }
-`
+`;
 H.Rank = styled.div`
   color: #cae962;
-  font-size: 16px;
-
+  font-size: 14px;
+  font-family: "Ubuntu", sans-serif;
+  font-weight: bolder;
+  text-transform: uppercase;
   @media screen and (max-width: 1200px) {
-    font-size: 14px;
+    font-size: 1px;
   }
   @media screen and (max-width: 480px) {
     font-size: 10px;
   }
-`
+`;
 H.Title = styled.h1`
+  font-family: "Ubuntu", sans-serif;
   color: #fff;
-  letter-spacing: 3px;
+  letter-spacing: 2px;
   margin-bottom: 20px;
+  font-size: 50px;
 
   @media screen and (max-width: 1200px) {
     font-size: 30px;
@@ -173,7 +182,7 @@ H.Title = styled.h1`
   @media screen and (max-width: 480px) {
     font-size: 16px;
   }
-`
+`;
 H.Icons = styled.div`
   display: flex;
   gap: 1em;
@@ -183,13 +192,13 @@ H.Icons = styled.div`
   @media screen and (max-width: 759px) {
     display: none;
   }
-`
+`;
 H.Icon = styled.p`
   display: flex;
   align-items: center;
   gap: 0.3em;
   color: #fff;
-`
+`;
 H.IconSpan = styled.span`
   display: inline-block;
   padding: 3px 4px;
@@ -199,14 +208,16 @@ H.IconSpan = styled.span`
   line-height: 1em;
   font-weight: 600;
   font-size: 12px;
-`
+`;
 H.Description = styled.div`
   display: -webkit-box;
   -webkit-line-clamp: 4;
   -webkit-box-orient: vertical;
   overflow: hidden;
+  font-family: "Ubuntu", sans-serif;
   color: #fff;
-  font-size: 12px;
+  font-size: 16px;
+  color: grey;
   margin: 2em 0;
 
   @media screen and (max-width: 1299px) {
@@ -216,7 +227,7 @@ H.Description = styled.div`
   @media screen and (max-width: 480px) {
     font-size: 11px;
   }
-`
+`;
 H.WatchBtn = styled.div`
   display: flex;
   align-items: center;
@@ -225,36 +236,38 @@ H.WatchBtn = styled.div`
   @media screen and (max-width: 320px) {
     flex-wrap: wrap;
   }
-`
+`;
 H.WatchLink = styled(Link)`
   display: flex;
   align-items: center;
   gap: 0.3em;
-  padding: 0.5em 0.6em;
+  padding: 0.7rem 1em;
   background-color: #cae962;
   color: #111;
-  font-size: 16px;
-  border-radius: 20px;
+  font-size: 18px;
+  border-radius: 30px;
+  font-family: "Ubuntu", sans-serif;
   @media screen and (max-width: 1200px) {
-    font-size: 14px;
+    font-size: 16px;
   }
   @media screen and (max-width: 480px) {
     font-size: 12px;
   }
-`
+`;
 H.DetailLink = styled(Link)`
   display: flex;
   align-items: center;
-  padding: 0.5em 0.6em;
+  padding: 0.7rem 1em;
   background-color: #4a4b51;
   color: #fff;
   gap: 0.3em;
-  font-size: 16px;
-  border-radius: 20px;
+  font-size: 18px;
+  border-radius: 30px;
+  font-family: "Ubuntu", sans-serif;
   @media screen and (max-width: 1200px) {
-    font-size: 14px;
+    font-size: 16px;
   }
   @media screen and (max-width: 480px) {
     font-size: 12px;
   }
-`
+`;
