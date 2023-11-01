7217096b6a5e3b590fb67aa8b881fedd7e2f4f6f
fiskryeziu <fiskryeziu@gmail.com>
2023-11-01 13:11:37 +0100

    added tooltip

--- src/components/CardTooltip/CardToolTip.jsx
+++ src/components/CardTooltip/CardToolTip.jsx
@@ -1,13 +1,138 @@
 import React from "react"
 import { CTooltip } from "./cardtooltip.style"
+import { data } from "../../data/mockData"
+import { FaPlay, FaStar } from "react-icons/fa"
+import { BiSolidCaptions, BiMicrophone } from "react-icons/bi"
+import { AiFillPlusCircle, AiOutlinePlus } from "react-icons/ai"
+
+const style = {
+  hd: {
+    background: "#ffdd95",
+    padding: ".1px 5px",
+    borderTopLeftRadius: "3px",
+    borderBottomLeftRadius: "3px",
+    fontSize: "12px",
+    display: "flex",
+    alignItems: "center",
+  },
+  sub: {
+    display: "flex",
+    background: "#b0e3af",
+    alignItems: "center",
+    fontSize: "11px",
+    padding: ".1px 5px",
+  },
+  dub: {
+    display: "flex",
+    background: "#e3b5cd",
+    alignItems: "center",
+    fontSize: "11px",
+    padding: ".1px 5px",
+    borderTopRightRadius: "3px",
+    borderBottomRightRadius: "3px",
+  },
+}
 
 const CardToolTip = ({ show, top }) => {
+  console.log(data)
   return (
-    <CTooltip.Wrapper
-      className="tooltip"
-      show={show}
-      top={top}
-    ></CTooltip.Wrapper>
+    <CTooltip.Wrapper className="tooltip" show={show} top={top}>
+      <CTooltip.Title>
+        Bleach: Thousand-Year Blood War - The Separation
+      </CTooltip.Title>
+      <CTooltip.Details>
+        <div
+          style={{
+            display: "flex",
+            gap: "2px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              gap: "5px",
+              paddingRight: "1em",
+            }}
+          >
+            <FaStar color="yellow" />{" "}
+            <p style={{ fontSize: "14px", color: "#fff" }}>
+              {data.details.rating}
+            </p>
+          </div>
+
+          <span style={style.hd}>HD</span>
+          <div style={style.sub}>
+            <BiSolidCaptions fontSize={16} />
+            <p>13</p>
+          </div>
+          <div style={style.dub}>
+            <BiMicrophone fontSize={16} />
+            <p>13</p>
+          </div>
+        </div>
+        <span style={style.hd}>TV</span>
+      </CTooltip.Details>
+      <CTooltip.Description>{data.description}</CTooltip.Description>
+      <CTooltip.Info>
+        <p>
+          <span
+            style={{
+              color: "#cccccc",
+            }}
+          >
+            Japanese :
+          </span>{" "}
+          {data.japanese}
+        </p>
+        <p>
+          <span
+            style={{
+              color: "#cccccc",
+            }}
+          >
+            Status:
+          </span>{" "}
+          {data.status}
+        </p>
+        <p>
+          <span
+            style={{
+              color: "#cccccc",
+            }}
+          >
+            Aired:
+          </span>{" "}
+          {data.aired}
+        </p>
+        <p>
+          <span
+            style={{
+              color: "#cccccc",
+            }}
+          >
+            Genres:
+          </span>{" "}
+          {data.genres.map((genre) => genre + ", ")}
+        </p>
+      </CTooltip.Info>
+      <CTooltip.BtnWrapper>
+        <CTooltip.Btn>
+          <FaPlay /> Watch now
+        </CTooltip.Btn>
+        <span
+          style={{
+            background: "#fff",
+            borderRadius: "50%",
+            padding: "5px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <AiOutlinePlus size={32} fill="black" />
+        </span>
+      </CTooltip.BtnWrapper>
+    </CTooltip.Wrapper>
   )
 }
 
--- src/components/CardTooltip/cardtooltip.style.js
+++ src/components/CardTooltip/cardtooltip.style.js
@@ -3,16 +3,70 @@ import styled from "styled-components";
 export const CTooltip = {}
 
 CTooltip.Wrapper = styled.div`
-display: ${({ show }) => (show ? 'block' : 'none')};
+opacity: ${({ show }) => (show ? '1' : '0')};
+display: flex;
+flex-direction: column;
 width: 300px;
-height: 300px;
-background-color: red;
+gap: .5em;
+padding:1em;
 position: absolute;
 top: ${({ top }) => top};
+transition:opacity 250ms ease-in;
 left: 50%;
 z-index: 10;
+word-wrap: break-word;
+border: none;
+background: rgba(78,78,82,.6);
+backdrop-filter: blur(10px);
+-webkit-backdrop-filter: blur(10px);
+border-radius: 7px;
+box-shadow: 0 20px 20px rgba(0,0,0,.05);
+
+
 
 @media screen and (max-width: 1278px) {
     display:none;
   }
+`
+
+CTooltip.Title = styled.p`
+color: #fff;
+font-weight:600;
+`
+CTooltip.Details = styled.div`
+display: flex;
+justify-content: space-between;
+`
+CTooltip.Description = styled.p`
+display: -webkit-box;
+-webkit-line-clamp: 3;
+-webkit-box-orient: vertical;
+overflow: hidden;
+font-family: "Poppins", sans-serif;
+font-size: 12px;
+margin: 2em 0;
+color: #cccccc;
+  `
+CTooltip.Info = styled.div`
+display: flex;
+flex-direction: column;
+gap: .3em;
+color: #fff;
+font-size: 12px;
+`
+CTooltip.BtnWrapper = styled.div`
+display: flex;
+gap: 1em;
+
+`
+CTooltip.Btn = styled.button`
+border-radius: 20px;
+width: 80%;
+display: flex;
+justify-content: center;
+align-items: center;
+gap: 1em;
+font-size: 13px;
+font-weight: 600;
+background-color: #ffdd95;
 `
